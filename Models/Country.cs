c45a761be999cfe2b171e52cb274538630c94092
YaroslavKNU2024 <86433715+YaroslavKNU2024@users.noreply.github.com>
2022-06-07 16:28:26 +0200

    Add files via upload

---
+++ Models/Country.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalCinema.Models
+{
+    public class Country
+    {
+        public Country()
+        {
+            Studios = new List<Studio>();
+            Cinemas = new List<Cinema>();
+        }
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Поле не може бути пустим")]
+        [Display(Name = "Країна")]
+        public string CountryName { get; set; }
+
+        public virtual ICollection<Studio> Studios { get; set; }
+        public virtual ICollection<Cinema> Cinemas { get; set; }
+    }
+}
